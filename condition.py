14b0e6a53cf241a4e5d5daee9f30721955788190
Rahul Singh <113496329+webdevelopre022@users.noreply.github.com>
2024-10-16 01:14:07 -0700

    Create condition.py

---
+++ condition.py
@@ -0,0 +1,11 @@
+Conditions in Python 
+Conditional output in python can be obtained by using if-else and elif (else if) statements. 
+
+a = 3
+b = 9
+if b % a == 0 :
+    print ("b is divisible by a")
+elif b + 1 == 10:
+    print ("Increment in b produces 10")
+else:
+    print ("You are in else statement")
