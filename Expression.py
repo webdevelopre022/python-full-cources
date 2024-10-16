e313b2428e132cdfbaa0ea2ccf052fa2ac114698
Rahul Singh <113496329+webdevelopre022@users.noreply.github.com>
2024-10-16 01:13:04 -0700

    Create Expression.py

---
+++ Expression.py
@@ -0,0 +1,9 @@
+Expressions in Python 
+Arithmetic operations in python can be performed by using arithmetic operators and some of the in-built functions. 
+
+a = 2
+b = 3
+c = a + b
+print (c)
+d = a * b
+print (d)
